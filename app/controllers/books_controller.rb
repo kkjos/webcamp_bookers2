3e4a195a8e59fea637ba0d65bdf73854b5194ee8
kkjos <k19951227@gmail.com>
2021-10-18 16:41:00 +0000

    [stop]book_params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
@@ -18,10 +19,14 @@ class BooksController < ApplicationController
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :books, only:[:index, :show, :new, :create, :destroy]
-  resources :users, only:[:show, :index]
+  resources :users, only:[:show, :index, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
