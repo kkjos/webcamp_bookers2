c5e1145d24a7fcf84bacfe4d83f78170fb984079
kkjos <k19951227@gmail.com>
2021-10-18 09:04:55 +0000

    [add]top/bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @books = @user.books.page(params[:page]).reverse_order
+    @books = @user.books.page(params[:id])
   end
-  
+
   def index
     @users = User.all
   end
