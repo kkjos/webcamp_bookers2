45e5f5b146c71c781e31f01c2cebdf1bb4684d2e
kkjos <k19951227@gmail.com>
2021-10-18 07:33:42 +0000

    [error]user/index.show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,27 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.save
+    @book.user_id = current_user.id
+    redirect_to books_path
   end
 
   def destroy
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @books = @user.books.page(params[:page]).reverse_order
+  end
+  
+  def index
+    @users = User.all
+  end
+
+    private
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
   end
 end
