6b9cfbc797c0ae426d0cb5be4f49290419d58512
kkjos <k19951227@gmail.com>
2021-10-19 12:31:50 +0000

    [add]バリデーション機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,7 +21,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-     redirect_to books_path
+     redirect_to book_path(book.id)
     else
       @books = Book.all
       @user = current_user
@@ -35,8 +35,11 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-  
+
   attachment :profile_image
+
+  validates :name, presence: true
 end
