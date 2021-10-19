cf94fbf8cb813496d3f71784ef79c7cac4366cab
kkjos <k19951227@gmail.com>
2021-10-19 03:12:07 +0000

    [add]遷移先設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,8 @@ class UsersController < ApplicationController
     redirect_to user_path(@user.id)
   end
 
-    private
+  private
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
