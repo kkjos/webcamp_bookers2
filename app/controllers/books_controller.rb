0958cfd838b720fcaea6110cce1c01ac0c1fcd62
kkjos <k19951227@gmail.com>
2021-10-19 12:04:16 +0000

    [add]books/show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,11 @@ class BooksController < ApplicationController
   end
 
   def show
+    @books = Book.all
+    @new_book = Book.new
     @book = Book.find(params[:id])
+    @user = current_user
+    @users = User.all
   end
 
   def new
@@ -19,10 +23,20 @@ class BooksController < ApplicationController
     if @book.save
      redirect_to books_path
     else
-      @book = Book.new(book_params)
-      @book.user_id = current_user.id
-      render :show
+      @books = Book.all
+      @user = current_user
+      render :index
     end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'homes' => 'homes#about', as:'homes_about'
   devise_for :users
 
-  resources :books, only:[:index, :show, :new, :create, :destroy]
-  resources :users, only:[:show, :index, :edit, :update]
+  resources :books
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
